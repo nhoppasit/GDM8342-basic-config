443d1a3537301b4e6acfcc7dcb6136089eaba496
Nhoppasit S <nhoppasit@gmail.com>
2024-08-11 15:01:40 +0700

    Test flyout page of DCV

--- src/GDM8342L1MauiApp/GDM8342L1MauiApp/Pages/DCV1/DCVFlyoutPage.xaml.cs
+++ src/GDM8342L1MauiApp/GDM8342L1MauiApp/Pages/DCV1/DCVFlyoutPage.xaml.cs
@@ -1,7 +1,23 @@
 namespace GDM8342L1MauiApp.Pages.DCV1;
 
-public partial class DCVFlyoutPage : ContentPage {
+public partial class DCVFlyoutPage : FlyoutPage {
     public DCVFlyoutPage() {
         InitializeComponent();
+    }
+
+    protected override void OnSizeAllocated(double width, double height) {
+        base.OnSizeAllocated(width, height);
+        // Check for orientation change and adjust layout accordingly
+        if (width > height) // Landscape mode
+        {
+            if (this.FlyoutLayoutBehavior != FlyoutLayoutBehavior.Split) {
+                this.FlyoutLayoutBehavior = FlyoutLayoutBehavior.Split;
+            }
+        } else // Portrait mode
+        {
+            if (this.FlyoutLayoutBehavior != FlyoutLayoutBehavior.Default) {
+                this.FlyoutLayoutBehavior = FlyoutLayoutBehavior.Default;
+            }
+        }
     }
 }
